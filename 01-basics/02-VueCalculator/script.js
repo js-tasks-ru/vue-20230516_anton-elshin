a15ebc2f9d5acc1e2bdfe911de6f79f4b8d62b8a
Anton Elshin <anton.elshin@mail.ru>
2023-05-22 17:54:50 +0300

    Solve calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,50 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const App = defineComponent({
+  name: 'App',
+
+  data() {
+    return {
+      calculator: {
+        digit_1: 0,
+        digit_2: 0,
+        operator: 'sum',
+      },
+    };
+  },
+
+  computed: {
+    result() {
+      let res = 0;
+
+      switch (this.calculator.operator) {
+        case 'subtract':
+          res = this.calculator.digit_1 - this.calculator.digit_2;
+          break;
+        case 'multiply':
+          res = this.calculator.digit_1 * this.calculator.digit_2;
+          break;
+        case 'divide':
+          if(this.calculator.digit_2 !== 0) {
+            res = this.calculator.digit_1 / this.calculator.digit_2;
+          }
+          else {
+            res = 'error';
+          }
+          break;
+        default:
+          res = this.calculator.digit_1 + this.calculator.digit_2;
+          break;
+      }
+
+      return res;
+    },
+  },
+});
+
+const app = createApp(App);
+
+const vm = app.mount('#app');
+
+window.vm = vm;
