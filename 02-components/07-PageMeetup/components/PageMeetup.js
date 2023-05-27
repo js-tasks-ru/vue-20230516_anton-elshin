9f02ce6f4e47c39812731b76c5fadb97d5d7ef02
Anton Elshin <anton.elshin@mail.ru>
2023-05-27 14:51:02 +0300

    Solve page meetup

--- 02-components/07-PageMeetup/components/PageMeetup.js
+++ 02-components/07-PageMeetup/components/PageMeetup.js
@@ -1,7 +1,8 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import MeetupView from "../../06-MeetupView/components/MeetupView";
+import { fetchMeetupById } from '../meetupService.js';
 
 export default defineComponent({
   name: 'PageMeetup',
@@ -9,18 +10,65 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupView,
+  },
+
+  props: {
+    meetupId: {
+      type: Number,
+      required: true,
+      default: 1
+    }
+  },
+
+  data() {
+    return {
+      meetup: null,
+      loading: true,
+      error: ''
+    }
+  },
+
+  methods: {
+    fetchMeetup(meetupId) {
+      this.meetup = null;
+      this.loading = true;
+      this.error = '';
+      fetchMeetupById(meetupId).then(
+        result => {
+          this.meetup = result;
+          this.loading = false;
+        },
+        error => {
+          this.loading = false;
+          this.error = error.toString().replace("Error: ", "");
+        }
+      );
+    },
+  },
+
+  watch: {
+    meetupId: {
+      handler(newValue, oldValue) {
+        this.fetchMeetup(newValue);
+      }
+    }
+  },
+
+  mounted() {
+    this.fetchMeetup(this.meetupId);
   },
 
   template: `
     <div class="page-meetup">
-      <!-- meetup view -->
+      <MeetupView v-if="meetup && !loading && error === ''" :meetup="meetup" />
 
-      <UiContainer>
+      <UiContainer v-if="loading">
         <UiAlert>Загрузка...</UiAlert>
       </UiContainer>
 
-      <UiContainer>
-        <UiAlert>error</UiAlert>
+      <UiContainer v-if="error !== ''">
+        <UiAlert>{{ error }}</UiAlert>
       </UiContainer>
     </div>`,
 });
