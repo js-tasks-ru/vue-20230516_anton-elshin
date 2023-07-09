db5e927f5b7146a11d0f130d9c9bd2fb8a43c636
Anton Elshin <anton.elshin@mail.ru>
2023-07-09 15:31:08 +0300

    Solve ui input date

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -28,31 +28,22 @@ export default {
     }
   },
 
-  watch: {
+/*  watch: {
     modelValue: {
       deep: true,
       immediate: true,
       handler() {
-        this.currentDate = this.modelValue ? new Date(new Date().setTime(this.modelValue)) : '';
+
       }
     }
-  },
-
-  data() {
-    return {
-      currentDate: this.modelValue ? new Date(new Date().setTime(this.modelValue)) : '',
-    }
-  },
+  },*/
 
   emits: ['update:modelValue'],
 
   computed: {
     modelValueProxy: {
       get() {
-        if(this.currentDate === '') {
-          return '';
-        }
-        const currentDateISO = this.currentDate.toISOString();
+        const currentDateISO = new Date(new Date().setTime(this.modelValue)).toISOString();
 
         if(this.type === 'date') {
           return currentDateISO.slice(0, 10);
@@ -70,64 +61,26 @@ export default {
         return null;
       },
 
-      set(value) {
-        if(this.currentDate === '') {
-          this.currentDate = new Date();
-          this.currentDate.setHours(this.currentDate.getTimezoneOffset()/60 * -1);
-          this.currentDate.setMinutes(0);
-          this.currentDate.setSeconds(0);
-          this.currentDate.setMilliseconds(0);
-        }
-
-        if(this.type === 'date') {
-          const yyyy = value.slice(0, 4);
-          const mm = parseInt(value.slice(5, 7)) - 1;
-          const dd = parseInt(value.slice(8, 10));
-
-          this.currentDate.setFullYear(yyyy);
-          this.currentDate.setMonth(mm);
-          this.currentDate.setDate(dd);
-        }
-        else if(this.type === 'time' && (!this.step || this.step % 60 === 0)) {
-          const hh = parseInt(value.slice(0, 2));
-          const mi = parseInt(value.slice(3, 5));
-
-          this.currentDate.setHours(hh);
-          this.currentDate.setMinutes(mi);
-        }
-        else if(this.type === 'time' && this.step) {
-          const hh = parseInt(value.slice(0, 2));
-          const mm = parseInt(value.slice(3, 5));
-          const ss = parseInt(value.slice(6, 8));
-
-          this.currentDate.setHours(hh);
-          this.currentDate.setMinutes(mm);
-          this.currentDate.setSeconds(ss);
-        }
-        else if(this.type === 'datetime-local') {
-          const yyyy = value.slice(0, 4);
-          const mm = parseInt(value.slice(5, 7)) - 1;
-          const dd = parseInt(value.slice(8, 10));
-          const hh = parseInt(value.slice(11, 13));
-          const mi = parseInt(value.slice(14, 16));
-
-          this.currentDate.setFullYear(yyyy);
-          this.currentDate.setMonth(mm);
-          this.currentDate.setDate(dd);
-          this.currentDate.setHours(hh);
-          this.currentDate.setMinutes(mi);
-        }
-
-        if(this.currentDate !== '') {
-          this.$emit('update:modelValue', this.currentDate.getTime());
-        }
-        else {
-          this.$emit('update:modelValue', '');
-        }
+      set() {
+        this.$emit('update:modelValue', this.valueAsNumber());
       }
     }
   },
 
   components: { UiInput },
+
+  methods: {
+    valueAsNumber() {
+      if(this.$refs.input?.$el) {
+        for(let i=0; i<this.$refs.input.$el.childNodes.length; i++) {
+          if(this.$refs.input.$el.childNodes[i].valueAsNumber) {
+            return this.$refs.input.$el.childNodes[i].valueAsNumber;
+          }
+        }
+      }
+
+      return this.modelValue;
+    },
+  },
 };
 </script>
